60b9e1ff80f1551143e724013974584abb04fb65
midoriknecht <107896292+midoriknecht@users.noreply.github.com>
2022-06-22 12:46:45 -0500

    Add files via upload

--- functions.py
+++ functions.py
@@ -2,11 +2,11 @@
 #  *****************************************************************************
 #    FILE:        functions.py
 #
-#    AUTHOR:      {Your Name Here}
+#    AUTHOR:      {Midori Knecht}
 #
 #    ASSIGNMENT:  Magic 8 Ball fortune teller and Eli's Shipping
 #
-#    DATE:         {Today's Date}
+#    DATE:         {06/22/2022}
 #
 #    DESCRIPTION: {Your Description Here}
 #
@@ -34,25 +34,73 @@
 # Over 6 lb but less than or equal to 10 lb |   $12.00          |	$0.00
 # Over 10 lb                                |   $14.25          |	$0.00
 
+from random import randrange 
 
-# Make sure to add parameters
-def fortune():
+def fortune(name, question):
+  answer = ["Yes - definitely.", "It is decidedly so.", "Without a doubt.", "Reply hazy, try again.", "Ask again       later.", "Better not tell you now.", "My soures say no.", "Outlook not so good.", "Very doubtful."]
+  randAns = randrange(0, 9)
+  fortune = answer[randAns]
 
-    # This ignores our code so that we do not get an error Remove 'pass' when you want to start testing code.
-    pass
+  space = name.find(" ")
+  fName = name[:space]
 
-#  Make sure to add parameters
-def shipping():
+  if (fName[-1] == "s"):
+    print(f"{fName}' Question: {question}")
+  else:
+    print(f"{fName}'s Question: {question}")
+  
+  print(f"Magic 8-Ball's answer: {fortune}")
+  print()
 
-    # This ignores our code so that we do not get an error Remove 'pass' when you want to start testing code.
-    pass
 
+def shipping(weight):
+  if (weight <= 2):
+    groundShip = weight * 1.5 + 20
+    groundPremShip = 125
+    droneShip = weight * 4.5
+  elif (weight > 2 and weight <= 6):
+    groundShip = weight * 3 + 20 
+    groundPremShip = 125
+    droneShip = weight * 9
+  elif (weight > 6 and weight <= 10):
+    groundShip = weight * 4 + 20 
+    groundPremShip = 125
+    droneShip = weight * 12
+  else:
+    groundShip = weight * 4.75 + 20 
+    groundPremShip = 125
+    droneShip = weight * 14.25
+    
+  print(f"The price of ground shipping is ${groundShip}.")
+  print(f"The price of ground shipping is ${groundPremShip}.")
+  print(f"The price of drone shipping is ${droneShip}.")
+
+  if (groundShip < groundPremShip and groundShip < droneShip):
+    best = "ground shipping"
+  elif (groundPremShip < groundShip and groundPremShip < droneShip):
+    best = "ground premium shipping"
+  elif (groundShip == groundPremShip and groundShip < droneShip):
+    best = "ground shipping or ground shipping premium"
+  elif (droneShip == groundShip and droneShip < groundPremShip): 
+    best = "drone shipping or ground shipping"
+  elif (droneShip == groundPremShip and droneShip < groundShip):
+    best = "drone shipping or ground premium shipping"
+  else:
+    best = "drone shipping"
+
+  print(f"To save the most money you should use {best}.")
 
 def main():
+# fortune
+  usersName = input("What's your name? ")
+  usersQuestion = input("What's your question? ")
+  fortune(usersName, usersQuestion)
 
+# shipping
+  packageWeight = float(input("How much does your package weigh in lbs? "))
+  shipping(packageWeight)
     # Collect user input in main and pass these values to the functions fortune and shipping. 
-    # This ignores our code so that we do not get an error Remove 'pass' when you want to start testing code.
-    pass
+    
 
 ##################DO NOT EDIT BELOW THIS LINE################
 # This invokes the main function.  It is always included in our
